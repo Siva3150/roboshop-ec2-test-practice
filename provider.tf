fb1b4bec2767d3cd482facc80a02b7e31af91a67
Siva prasad <sivaprasad3150@gmail.com>
2026-03-01 03:41:52 +0530

    terraform

---
+++ provider.tf
@@ -0,0 +1,23 @@
+terraform{
+    required_providers {
+      aws = {
+        source = "hashicorp/aws"
+        version = "6.33.0"
+        
+      }
+    }
+
+  backend "s3" {
+    bucket = "86s-siva-remote-state"
+    key    = "ec2-module-test"
+    region = "us-east-1"
+    use_lockfile = true 
+    encrypt = true 
+  }
+}
+
+
+
+provider "aws" {
+   region = "us-east-1"
+}
